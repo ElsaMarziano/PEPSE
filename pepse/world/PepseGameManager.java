94c3accf9ce1397f3cc192d957e755d7cac15e85
Elsa Marziano <elsasebagh@gmail.com>
2024-03-01 12:53:20 +0200

    added sky

---
+++ pepse/world/PepseGameManager.java
@@ -0,0 +1,24 @@
+package pepse.util.pepse.world;
+
+import danogl.GameManager;
+import danogl.GameObject;
+import danogl.collisions.Layer;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+
+public class PepseGameManager extends GameManager {
+    public static void main(String[] args) {
+        new PepseGameManager().run();
+    }
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        GameObject sky = Sky.create(windowController.getWindowDimensions());
+        // TODO In pdf they said to add it to "skylayer" but no idea what that is
+        gameObjects().addGameObject(sky, Layer.BACKGROUND);
+
+    }
+}
