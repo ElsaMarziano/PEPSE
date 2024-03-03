c320f114fa04502fc7728595e48671352fd0a87a
archsak <aharon.saksonov@mail.huji.ac.il>
2024-03-03 07:50:10 +0200

    sunHalo

--- pepse/world/daynight/SunHalo.java
+++ pepse/world/daynight/SunHalo.java
@@ -1,4 +1,28 @@
 package pepse.util.pepse.world.daynight;
 
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.OvalRenderable;
+import danogl.util.Vector2;
+import danogl.GameObject;
+import java.awt.*;
+
+
 public class SunHalo {
+    private static final float HALO_SIZE = 100;
+    public static GameObject create(GameObject sun){
+        Vector2 initialPosition = sun.getCenter();
+        float haloSize = HALO_SIZE;
+        OvalRenderable haloRenderable = new OvalRenderable(new Color(255, 255, 0, 20));
+
+        GameObject sunHalo = new GameObject(initialPosition, new Vector2(haloSize, haloSize),
+                haloRenderable);
+
+        sunHalo.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+        sunHalo.setTag("sunHalo");
+
+        sunHalo.addComponent(deltaTime -> sunHalo.setCenter(sun.getCenter()));
+
+        return sunHalo;
+    }
+
 }
